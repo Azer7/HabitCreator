048307833a009755ba51a91971639d1566b7546a
Azer7 <Azer7@users.noreply.github.com>
2018-11-18 01:48:05 -0700

    s

--- goalsPR.js
+++ goalsPR.js
@@ -1,11 +1,22 @@
 function initGoalsPR() {
-    var data = [
+  var currentDay = new Date().getDate();
+  var dayOfTheWeek = new Date().getDay();
+  var sundayDay = currentDay - dayOfTheWeek;
+  var data = [
         {
           x: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-          y: [20, 14, 23],
+          y: getWeekSleepTime(sundayDay),
           type: 'bar'
         }
       ];
 
     Plotly.newPlot('sleepPlot', data);
+}
+
+function getWeekSleepTime(sundayDay) {
+  var sleepTimes = [];
+  for(var i = 0; i < 7; i++) {
+    sleepTimes.push(days[sundayDay + i]);
+  }
+  return sleepTimes;
 }
