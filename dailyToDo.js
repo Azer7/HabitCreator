16d6264fed41276bcf5d50e70eff3a927c6f425b
Xyrtur <45132330+Xyrtur@users.noreply.github.com>
2018-11-17 21:25:54 -0700

    aaaaaah

--- dailyToDo.js
+++ dailyToDo.js
@@ -1,8 +1,54 @@
-function myfunction(){
-    var x = $("#mycheck");
-    var d = new Date(year, month, day)
-    if (x.checked){
-        points +=5
+// var data;
+// $.get(data.json, function(e) {
+// data = e;
+// });
+var days = [];
+
+class notDay {
+    constructor() {
+        this.didntLookAtScreen = false;        
+        this.submittedSleep = false;
+        this.drunk = false;
+        this.submittedDrunk = false;
+    }
+}
+
+function resetData() {
+    for(var i = 0; i < 31; i++) {
+        days.push(new notDay());
+    }
+}
+resetData();
+
+function initDailyToDo(){
+    var myCheck = $("#mycheck");
+    var myCheck2 = $("#mycheck2");
+    var d = new Date()
+    var currentDay = d.getDate();
+    if (days[currentDay].submittedSleep){
+        myCheck.prop('disabled', true);        
+    } else {
+        myCheck.prop('disabled', false);        
+    }
+
+    if(days[currentDay].submittedDrunk) {
+        myCheck2.prop('disabled', true);
+    } else {
+        myCheck2.prop('disabled', false);        
     }
     
 }
+
+function submitDaily() {
+    var myCheck = $("#mycheck");
+    var myCheck2 = $("#mycheck2");
+    var d = new Date()
+    var currentDay = d.getDate();
+    days[currentDay].submittedSleep = myCheck.is(":checked");
+    days[currentDay].submittedDrunk = myCheck2.is(":checked");
+    initDailyToDo();
+}
+
+
+
+
