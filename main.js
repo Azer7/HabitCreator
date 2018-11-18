cf3b8aefdbd4bb64b0d29a51bcd143a1f7e0d4b4
Spazda08 <s.johnsensollos@gmail.com>
2018-11-17 22:28:10 -0700

    twelfth commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
 var tabPanels=document.querySelectorAll(".tabContainer .tabPanel");
-
+var points=
 function showPanel(panelIndex, colorCode) {
     var fileToLoad = "";
     switch(panelIndex) {
