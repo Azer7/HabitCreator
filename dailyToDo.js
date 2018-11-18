5a561031741424c6eb6be115b4ac42e3d6d11d4c
Xyrtur <45132330+Xyrtur@users.noreply.github.com>
2018-11-18 01:08:12 -0700

    what

--- dailyToDo.js
+++ dailyToDo.js
@@ -17,11 +17,13 @@ input.change(function() {
 
 
 function initDailyToDo(){
+    console.log("I am here");
     var myCheck = $("#mycheck");
     var myCheck2 = $("#mycheck2");
+    var myInput = $("#hoursSlept")
     var d = new Date()
     var currentDay = d.getDate();
-    if (days[currentDay].submittedSleep){
+    if (days[currentDay].submittedScreen){
         myCheck.prop('disabled', true);      
     } else {
         myCheck.prop('disabled', false);        
@@ -44,23 +46,32 @@ function initDailyToDo(){
     } else {
         myCheck2.prop("checked", false);
     }
+
+    if(days[currentDay].submittedTimeSlept){
+        myInput.prop('disabled', true);
+    } else {
+        myInput.prop('disabled', false);
+    }
     
+    if(days[currentDay].timeSlept) {
+        myCheck2.val(days[currentDay].timeSlept);
+    } 
 }
 
 function submitDaily() {
     var myCheck = $("#mycheck");
     var myCheck2 = $("#mycheck2");
+    var myInput = $("#hoursSlept");
     var d = new Date()
     var currentDay = d.getDate();
     days[currentDay].didntLookAtScreen = myCheck.is(":checked");
     days[currentDay].drunk = myCheck2.is(":checked");
-    days[currentDay].submittedSleep = true;
+    days[currentDay].timeSlept = myInput.val() ? myInput.val() : 0;
+    days[currentDay].submittedScreen = true;
     days[currentDay].submittedDrunk = true;
+    days[currentDay].submittedSleepTime = true;
+    
     localStorage.days = JSON.stringify(days);
     updatePoints();
     initDailyToDo();
 }
-
-
-
-
