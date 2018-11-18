a5438365b1ae030b4d756fc5e8a8d5d0ea064124
Spazda08 <s.johnsensollos@gmail.com>
2018-11-17 18:30:06 -0700

    sixth commit

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button"
 var tabPanels=document.querySelectorAll(".tabContainer .tabPanel");
 function showPanel(panelIndex, colorCode) {
     tabButtons.forEach(function(node){
-        node.style.backgroundColor=" ";
-        node.style.Color=" ";
+        node.style.backgroundColor="";
+        node.style.color="";
 });
 tabButtons[panelIndex].style.backgroundColor=colorCode;
-tabButtons[panelIndex].style.Color="white";
+tabButtons[panelIndex].style.color="white";
 tabPanels.forEach(function(node){
     node.style.display="none";
 });
